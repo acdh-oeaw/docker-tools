fefa49d8c92b5c389d099435d173f6a3e2ad1f3a
root <root@eos.arz.oeaw.ac.at>
2015-10-19 09:56:53 +0200

    Fixed #4895 - only free ports are no assigned (check is perforemd by calling netstat)

--- create-virtual-host.py
+++ create-virtual-host.py
@@ -15,6 +15,12 @@ class HTTPReverseProxy(object):
   @staticmethod
   def getPort():
     HTTPReverseProxy.portNumber += 1
+
+    # find first free port
+    ports = subprocess.check_output(['/bin/netstat', '-lt', '--numeric-ports'])
+    while re.search(':' + str(HTTPReverseProxy.portNumber) + ' ', ports) :
+      HTTPReverseProxy.portNumber += 1
+
     return HTTPReverseProxy.portNumber
 
 class Param(object):
