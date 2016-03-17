28f63b6919b1bc92330580a10211047356a7ebdd
Omar Siam <omar.siam@oeaw.ac.at>
2016-03-17 14:56:16 +0100

    Register new Environments. Make LogDir mandatory for corpus_shell.

--- docker_manage/Environments/Environmentcorpus_shell.py
+++ docker_manage/Environments/Environmentcorpus_shell.py
@@ -3,5 +3,7 @@ from . import *
 class Environmentcorpus_shell(EnvironmentPHP, IEnvironment):
   def __init__(self, conf, owner):
     if 'DockerfileDir' not in conf :
-      conf['DockerfileDir'] = self.DockerfileDir
+      conf['DockerfileDir'] = 'corpus_shell'
     super(Environmentcorpus_shell, self).__init__(conf, owner)
+    if self.LogDir is None:
+      raise Exception('LogDir is required for this environment. It is missing or invalid')
--- docker_manage/Environments/__init__.py
+++ docker_manage/Environments/__init__.py
@@ -7,7 +7,7 @@ from .EnvironmentApache import EnvironmentApache
 from .EnvironmentPHP import EnvironmentPHP
 from .EnvironmentWSGI3 import EnvironmentWSGI3
 from .EnvironmentWSGI2 import EnvironmentWSGI2
-from .EnvironmentDrupal7 import EnvironmentDrupal7
+from .EnvironmentDrupal7 import EnvironmentDrupal7, EnvironmentDrupal7old
 from .EnvironmentJava8 import EnvironmentJava8
 from .EnvironmentExistdb30 import EnvironmenteXistdb30
 from .Environmentcrxqmets import Environmentcrxqmets
@@ -15,3 +15,4 @@ from .EnvironmentBaseX import EnvironmentBaseX, EnvironmentRHELBaseX
 from .EnvironmentSkeBase import EnvironmentSkeBase
 from .EnvironmentSketchEngine import EnvironmentSketchEngine
 from .EnvironmentNoske import EnvironmentNoSkE, EnvironmentNoSkE_patched
+from .Environmentcorpus_shell import Environmentcorpus_shell
