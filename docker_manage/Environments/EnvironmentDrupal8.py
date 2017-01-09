440fdb81f4531b15fd80ab9e2e279361fd862116
root <root@apollo.arz.oeaw.ac.at>
2017-01-09 13:16:15 +0100

    Default Drupal8 version raised to 2.5

--- docker_manage/Environments/EnvironmentDrupal8.py
+++ docker_manage/Environments/EnvironmentDrupal8.py
@@ -5,7 +5,7 @@ from . import *
 
 class EnvironmentDrupal8(EnvironmentPHP, IEnvironment):
     skipDocumentRoot = True
-    Version = '2.4'
+    Version = '2.5'
     VendorDir = False
 
     def __init__(self, conf, owner):
