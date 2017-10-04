ac4a66d6f00aa50e3e9d5d7a3137577b762604d9
Dalibor Pancic <dalibor.pancic@oeaw.ac.at>
2017-10-04 11:12:00 +0200

    IIIF Environment refactored.

--- docker_manage/Environments/EnvironmentIIIF.py
+++ docker_manage/Environments/EnvironmentIIIF.py
@@ -1,15 +1,12 @@
 from . import *
 
-class EnvironmentIIIF(EnvironmentApache, IEnvironment):
-  skipDocumentRoot   = True 
-  DocumentRootMount  = '/usr/share/cgi-bin'
-  DocumentRoot       = None
+class EnvironmentIIIF(EnvironmentHTTP, IEnvironment):
   DockerfileDir = 'iiif'
   DropzoneDirMount = '/dropzone/'
   DropzoneDir      = None
   DataDirMount     = '/data/'
   DataDir          = None
-  LogDirMount      = '/opt/iiifserver/logs'
+  LogDirMount      = '/var/log/nginx'
   LogDir           = None
 
   def __init__(self, conf, owner):
@@ -52,73 +49,7 @@ class EnvironmentIIIF(EnvironmentApache, IEnvironment):
     self.Mounts.append({ "Host" : self.DropzoneDir, "Guest" : self.DropzoneDirMount, "Rights" : "rw" })
     self.Mounts.append({ "Host" : self.DataDir, "Guest" : self.DataDirMount, "Rights" : "rw" })
     self.Mounts.append({ "Host" : self.LogDir, "Guest" : self.LogDirMount, "Rights" : "rw" })
+  
 
-  def apacheConfigure(self):
-    tmpFile = '/tmp/' + self.Name
-    with open(tmpFile, 'w') as vhFile:
-      vhFile.write(self.guestVHTemplate.format(
-        ServerName = self.ServerName,
-        ServerAlias = self.getServerAlias(),
-        UID = '#' + str(self.UID),
-        GID = '#' + str(self.GID),
-        DocumentRootMount = self.DocumentRootMount,
-        AllowOverride = self.AllowOverride,
-        Options = self.Options,
-        Aliases = self.getAliases(),
-        FastCgiExternalServer = self.FastCgiExternalServer(),
-        ImitateHTTPS = self.imitateHTTPSTemplate if self.ImitateHTTPS == 'true' else ''
-      ))
-    self.runProcess(['docker', 'cp', tmpFile, self.Name + ':/etc/apache2/sites-enabled/000-default.conf'], False, '', 'Apache config update in guest failed')
-    os.remove(tmpFile)
-
-  def apacheRestart(self, verbose):
-    self.runProcess(['docker', 'exec', self.Name, 'apachectl', 'graceful'], verbose, '', 'Apache graceful restart failed')
-    self.runProcess(['docker', 'exec', self.Name, 'supervisorctl', 'restart', 'apache2'], verbose, '', 'Apache restart failed')
-
-  def getDockerOpts(self):
-    dockerOpts = super(EnvironmentApache, self).getDockerOpts()
-    dockerOpts += ['--cap-add=SYS_NICE', '--cap-add=DAC_READ_SEARCH']
-    return dockerOpts
-
-  def getAddHandler(self):
-    return 'fastcgi-script fcg fcgi fpl'  
-
-  def getAliases(self):
-    aliases = ''
-    for alias in self.Aliases:
-      aliases += 'Alias ' + alias['Alias'] + ' ' + self.DocumentRootMount + '/' + alias['Path'] + '\n'
-    return aliases
-
-  def getFastCgiExternalServer(self):
-    return '/usr/share/cgi-bin/iiifserver.fcgi -host 127.0.0.1:9000 -idle-timeout 3600'  
-
-  def getGuestHomeDir(self):
-    return '/var/www/html'
-
-  imitateHTTPSTemplate = """
-  SetEnv HTTPS on
-  SetEnv REQUEST_SCHEME https
-  SetEnv protossl s
-"""
-
-  guestVHTemplate = """
-<VirtualHost *:80>
-  ServerName {ServerName}
-  DocumentRoot {DocumentRootMount}
-  ServerAlias {ServerAlias}
-  AssignUserID {UID} {GID}
-  FastCgiExternalServer {FastCgiExternalServer}
-
-  <Directory {DocumentRootMount}>
-    Require all granted
-    AllowOverride {AllowOverride}
-    Options {Options}
-  </Directory>
-   
-  {AddHandler} 
-  {Aliases}
-  {ImitateHTTPS}
-</VirtualHost>   
-"""  
 
 
