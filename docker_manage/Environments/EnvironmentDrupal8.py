61b195c60349a031415733a293b60cfcf2627d28
Dalibor Pancic <dalibor.pancic@oeaw.ac.at>
2017-11-02 12:15:48 +0100

    Drupal 8.4.1 added.

--- docker_manage/Environments/EnvironmentDrupal8.py
+++ docker_manage/Environments/EnvironmentDrupal8.py
@@ -5,7 +5,7 @@ from . import *
 
 class EnvironmentDrupal8(EnvironmentPHP, IEnvironment):
     skipDocumentRoot = True
-    Version = '4.0'
+    Version = '4.1'
     VendorDir = False
 
     def __init__(self, conf, owner):
@@ -115,7 +115,8 @@ class EnvironmentDrupal8(EnvironmentPHP, IEnvironment):
             '3.5': '23832fcfe0c6398727d24e582042f149',
             '3.6': 'de7ceedf9391c4b52c1c5a7f66809129',
             '3.7': 'e7b1f382d6bd2b18d4b4aca01d335bc0',
-            '4.0': '074795a2f5fc0b599a7dcfb9d1fb03f5'
+            '4.0': '074795a2f5fc0b599a7dcfb9d1fb03f5',
+            '4.1': '1c1db36ac5217f315bf9c03d64529f10'
         }
 
         if self.Version not in hashes:
