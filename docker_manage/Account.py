c6ca6a4aa7fcfd5e530d6c738edaf972847a0bc2
Dalibor Pancic <dalibor.pancic@oeaw.ac.at>
2018-01-10 13:25:47 +0100

    fixed typo error in environment message.

--- docker_manage/Account.py
+++ docker_manage/Account.py
@@ -62,7 +62,7 @@ class Account:
           raise Exception('environment has no type')
         self.environments.append(env)
       except Exception as e:
-        print '    Error in emvironment ', (1 + len(self.environments)), ': ', e
+        print '    Error in environment ', (1 + len(self.environments)), ': ', e
 
   def check(self, duplDomains, duplPorts, duplNames, names):
     errors = {}
