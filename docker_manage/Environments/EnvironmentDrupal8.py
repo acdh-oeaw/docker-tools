f96d585a458a7b9dff7496145a72532fda7c8200
Dalibor Pancic <dalibor.pancic@oeaw.ac.at>
2019-09-10 08:22:46 +0200

    Drupal 8.7.7 added

--- docker_manage/Environments/EnvironmentDrupal8.py
+++ docker_manage/Environments/EnvironmentDrupal8.py
@@ -5,7 +5,7 @@ from . import *
 
 class EnvironmentDrupal8(EnvironmentPHP, IEnvironment):
     skipDocumentRoot = True
-    Version = '7.6'
+    Version = '7.7'
     VendorDir = False
 
     def __init__(self, conf, owner):
@@ -166,7 +166,8 @@ class EnvironmentDrupal8(EnvironmentPHP, IEnvironment):
             '7.3': 'bba896a1ed2fbe4a1744d82c036fcfc4',
             '7.4': '5bd5db96c8a4d017e940a43285f5166b',
             '7.5': '39cc326d9db1b4acce9b8716193189fd',
-            '7.6': '484530915cbf6d96cb03379e3dda018e'
+            '7.6': '484530915cbf6d96cb03379e3dda018e',
+            '7.7': 'eda95eb4c6567049c661818ffe800d11'
 
 
         }
